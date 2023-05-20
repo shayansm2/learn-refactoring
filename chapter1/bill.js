13216ef410a892e1b5280fe0f492edef3673b77f
shayansm2 <shayansm09@gmail.com>
2023-05-20 18:57:59 +0330

    extract method

--- chapter1/bill.js
+++ chapter1/bill.js
@@ -8,10 +8,7 @@ export function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         // print line for this order
         result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
@@ -44,6 +41,13 @@ export function statement(invoice, plays) {
             default:
                 throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
+        return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
         return result;
     }
 }
