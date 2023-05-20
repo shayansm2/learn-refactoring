cbac03dbf77768eaf98b0f842b96583ee7cfe3f3
shayansm2 <shayansm09@gmail.com>
2023-05-20 19:08:57 +0330

    Split Loop

--- chapter1/bill.js
+++ chapter1/bill.js
@@ -4,12 +4,15 @@ export function statement(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // print line for this order
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
+
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
