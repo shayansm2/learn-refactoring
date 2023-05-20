7c8caffcdf93a8287c711b0b9a7e18c4c5f8406a
shayansm2 <shayansm09@gmail.com>
2023-05-20 19:21:46 +0330

    slide statement and split loop

--- chapter1/bill.js
+++ chapter1/bill.js
@@ -1,11 +1,14 @@
 export function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
+    let totalAmount = 0;
     for (let perf of invoice.performances) {
         // print line for this order
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
+    }
+
+    for (let perf of invoice.performances) {
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     }
 
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
