52a3b108170242af100b1d6e16b297b437ec5c12
shayansm2 <shayansm09@gmail.com>
2023-05-20 19:39:13 +0330

    extract method

--- chapter1/bill.js
+++ chapter1/bill.js
@@ -1,4 +1,4 @@
-export function statement(invoice, plays) {
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
@@ -6,6 +6,10 @@ export function statement(invoice, plays) {
     result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+}
+
+export function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
