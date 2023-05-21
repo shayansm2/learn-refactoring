f2e253e9759c58e043aa2dc896d3af8080514e04
shayansm2 <shayansm09@gmail.com>
2023-05-21 15:38:08 +0330

    display the html format of the invoice

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,7 +1,8 @@
 import {plays} from "./plays.json.js"
 import {invoices} from "./invoices.json.js";
-import {statement} from "./bill.js";
+import {htmlStatement, statement} from "./bill.js";
 
 console.log(plays)
 console.log(invoices)
 console.log(statement(invoices[0], plays))
+document.getElementById("np").innerHTML = htmlStatement(invoices[0], plays)
