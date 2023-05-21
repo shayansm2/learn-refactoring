7453f6da4c565586f0e75fe8c29a1c5b6dee927f
shayansm2 <shayansm09@gmail.com>
2023-05-21 14:38:09 +0330

    use dto fields instead of recalculating

--- chapter1/bill.js
+++ chapter1/bill.js
@@ -51,7 +51,7 @@ export function statement(invoice, plays) {
     function volumeCreditsFor(aPerformance) {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
     }
 
